1c7507d54b3d4c8b440cf634bda656918749dd79
Manishkumar1114 <162983418+Manishkumar1114@users.noreply.github.com>
2024-08-10 22:45:38 +0530

    Simple form creation using middlewares.

--- middleWare.js
+++ middleWare.js
@@ -1,18 +1,31 @@
- const http = require("http");
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
 
-app.use((req, res , next) => {
-  console.log('I am in MiddleWare');
-  next();
-})
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use((req, res , next) => {
-  console.log('I am in another MiddleWare');
+// Route to display the form
+app.use('/add-product', (req, res, next) => {
+  res.send(`
+    <form action="/product" method="POST">
+      <input type="text" name="title" placeholder="Product Title">
+      <input type="text" name="size" placeholder="Product Size">
+      <button type="submit">Add Product</button>
+    </form>
+  `);
+});
+
+// Route to handle form submission
+app.use('/product', (req, res, next) => {
+  console.log(req.body); 
+  res.redirect('/');
+});
+
+// Route for the homepage
+app.use('/', (req, res, next) => {
   res.send('<h1>Hellooooo</h1>');
-})
+});
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
